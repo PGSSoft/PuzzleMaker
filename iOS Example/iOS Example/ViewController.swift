75511abbcf649fe51d37164dca222c239fa5cda4
Kelly Roach <kellybrianroach@outlook.com>
2018-09-29 17:57:29 -0700

    Demo app resize image / numColumns / numRows for given device.

--- iOS Example/iOS Example/ViewController.swift
+++ iOS Example/iOS Example/ViewController.swift
@@ -11,18 +11,29 @@ import PuzzleMaker
 
 class ViewController: UIViewController {
 
-    static let numRows = 5
-    static let numColumns = 7
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        let puzzleMaker = PuzzleMaker(image: UIImage(named: "image")!, numRows: ViewController.numRows, numColumns: ViewController.numColumns)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let bounds = self.view.bounds
+        let imgDim = CGFloat(100.0)
+        let numColumns = Int(round(bounds.width/imgDim))
+        let numRows = Int(round(bounds.height/imgDim))
+        var image = UIImage(named: "image")!
+        // Resize image for given device.
+        UIGraphicsBeginImageContextWithOptions(bounds.size, _: false, _: 0.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height))
+        image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        // Make puzzle.
+        let puzzleMaker = PuzzleMaker(image: image, numRows: numRows, numColumns: numColumns)
         puzzleMaker.generatePuzzles { (throwableClosure) in
             do {
                 let puzzleElements = try throwableClosure()
-                for row in 0 ..< ViewController.numRows {
-                    for column in 0 ..< ViewController.numColumns {
+                for row in 0 ..< numRows {
+                    for column in 0 ..< numColumns {
                         let puzzleElement = puzzleElements[row][column]
                         let position = puzzleElement.position
                         let image = puzzleElement.image
